4be7c86df21bebb6dbbf13efceb3a332ef292926
Lakshmi <lakshmithotakura417@gmail.com>
2026-03-17 11:07:05 +0530

    home image added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import Icons from "./Icons";
+import homepage from "./assets/homepage.avif";
 const Home = () => {
   return (
     <div className="w-full md:py-[20px] py-[10px]">
@@ -22,7 +23,7 @@ const Home = () => {
         </div>
         <div className="basis-[50%] h-full">
           <img
-            src="https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={homepage}
             alt=""
             className="w-full h-full object-cover rounded-xl"
           />
